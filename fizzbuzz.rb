675f8e12047e14ac7d813c87812105c40b903568
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-09-29 09:28:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-    if if x %3 == 0
+    if x %3 == 0
         return "Fizz"
     elsif x %5 == 0
         return "Buzz"
-    elsif(x % 3 == 0 && x % 5 == 0)
+    elsif x % 3 == 0 && x % 5 == 0
         return "FizzBuzz"
     else 
         return nil
